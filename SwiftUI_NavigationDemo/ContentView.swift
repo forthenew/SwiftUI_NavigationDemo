de9b3f1013e717bea07717c6351bb0b7233183bc
forthenew <forthenew@gmail.com>
2021-01-10 11:01:41 +0900

    Initial Commit

---
+++ SwiftUI_NavigationDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_NavigationDemo
+//
+//  Created by stam on 2021/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_NavigationDemo/SwiftUI_NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationDemoApp.swift
+//  SwiftUI_NavigationDemo
+//
+//  Created by stam on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
